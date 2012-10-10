30b14222d18d5c4f1e196cf1bc29e19705056638
Rob Hilgefort <rjhilgefort@gmail.com>
2012-10-10 13:05:45 -0400

    missed changes related to the restructuring

---
+++ src/lib/lib-config.php
@@ -0,0 +1,107 @@
+<?php
+
+class config {
+  
+  private $debug = false;
+  private $templateFiles = "/twig";
+  private $phpFiles = "/php";
+  
+  /**
+   *
+   * @param type $includeAppConfig
+   * @param type $loadTwig
+   * @param type $ptr
+   * @return type
+   * @throws Exception 
+   */
+  function __construct() {
+    $this->root = dirname(__DIR__);
+  }
+
+  public function load($includeAppConfig=true, $loadTwig=true){
+    try
+    { 
+      //includeAppConfig
+      if($includeAppConfig === false){
+        //do nothing
+      }elseif($includeAppConfig === true){
+        require_once $this->root . $this->phpFiles . "/main.php";
+      }else{
+        throw new Exception("includeAppConfig parameter was unexpected");
+      }
+      
+       //loadTwig
+      if($loadTwig === false){
+        $rtn = true;
+      }elseif($loadTwig === true){
+        $rtn = $this->loadTwig();
+      }else{
+        throw new Exception("loadTwig array parameter was unexpected");
+        $rtn = false;
+      }
+      return $rtn;
+    }
+    catch (Exception $e)
+    {
+      echo "<pre>Error in config::load() ---  ".$e->getMessage()."</pre>";
+    } 
+  }
+  
+  public function destroy() {
+    self::rrmdir($this->root . $this->templateFiles . "/lib");
+  }
+  
+  /**
+   *
+   * @param type $ptr
+   * @return \Twig_Environment 
+   */
+  private function loadTwig () {
+    //move templates over
+    self::copy_directory($this->root."/lib/TwigTemplates", $this->root . $this->templateFiles . "/lib");
+    
+    require_once $this->root."/lib/Twig/Autoloader.php";
+    Twig_Autoloader::register();
+    $loader = new Twig_Loader_Filesystem($this->root . $this->templateFiles);
+    $twig = new Twig_Environment($loader, array(
+              'debug' => $this->debug,
+              'cache' => $this->root.'/lib/cache',
+              'auto_reload' => true));
+    
+    return $twig;
+  }
+  
+  public static function copy_directory( $source, $destination ) {
+    if ( is_dir( $source ) ) {
+      @mkdir( $destination );
+      $directory = dir( $source );
+      while ( FALSE !== ( $readdirectory = $directory->read() ) ) {
+        if ( $readdirectory == '.' || $readdirectory == '..' ) {
+          continue;
+        }
+        $PathDir = $source . '/' . $readdirectory; 
+        if ( is_dir( $PathDir ) ) {
+          copy_directory( $PathDir, $destination . '/' . $readdirectory );
+          continue;
+        }
+        copy( $PathDir, $destination . '/' . $readdirectory );
+      }
+
+      $directory->close();
+    }else {
+      copy( $source, $destination );
+    }
+  }
+  
+  public static function rrmdir($dir) {
+    foreach(glob($dir . '/*') as $file) {
+      if(is_dir($file))
+        rrmdir($file);
+      else
+        unlink($file);
+    }
+    rmdir($dir);
+  }
+  
+}
+?>
